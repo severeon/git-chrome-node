16a7772bc2d31a2eee40b620cbb51ad78acbe82a
alberte <dropped.on.japan@gmail.com>
2013-08-07 18:49:07 -0500

    Added express and routes handling.

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = require( 'express' ).express();
+var fs = require('fs');
+var _ = require('underscore');
+
+app.get('/', function(req, res) {  res.send( "Local server is running.  Might I suggest calling some simple /api routes?" );  });
+
+
+fs.readdir( './routes', function(err, files) {
+  console.log('Getting Routes');
+
+  _.each(files, function(file, fIdx) {
+    require('./routes/' + file)(app);
+  });
+});
+
+app.listen( process.argv[2] || 3000, function() {
+  console.log( 'Listening on port ' +( process.argv[2] || 3000 )+ '' );
+});
